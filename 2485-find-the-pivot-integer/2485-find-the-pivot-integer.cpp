bbe50eeb01c1c3c8a9438be6915aaba46de024c3
Tushar Khatri <132511116+TusharKhatrii@users.noreply.github.com>
2026-03-10 18:11:39 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,25 +1,10 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        int leftSum = 1, rightSum = n, res = -1;
-        int low = 2, high = n - 1;
-        while (low <= high) {
-            if (leftSum < rightSum) {
-                leftSum += low;
-                low++;
-            } else if (rightSum < leftSum) {
-                rightSum += high;
-                high--;
-            } else {
-                leftSum += low;
-                rightSum += high;
-                low++;
-                high--;
-            }
-        }
-        if (leftSum == rightSum && low-high == 2) {
-            res = high+1;
-        }
-        return res;
+        double sum = sqrt(n*(n+1)/2);
+        int res = (int) sum;
+        if(sum * sum == (n*(n+1)/2))
+        return sum;
+        return -1;
     }
 };
