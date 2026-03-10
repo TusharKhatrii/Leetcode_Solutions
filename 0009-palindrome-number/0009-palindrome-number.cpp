93f0cdb052e797930060d042c873dd2cc1a5d749
Tushar Khatri <132511116+TusharKhatrii@users.noreply.github.com>
2026-03-10 18:25:22 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -4,11 +4,10 @@ public:
         if (x < 0) {
             return false;
         }
-        int val=x;
+        int val = x;
         long long num = 0;
         while (x > 0) {
-            num *= 10;
-            num += (x % 10);
+            num = (num * 10) + (x % 10);
             x /= 10;
         }
         return (val == num);
