91c757478741d184580a9b3ac7166bc279c5dec1
Tushar Khatri <132511116+TusharKhatrii@users.noreply.github.com>
2026-03-12 15:43:05 +0500

    Time: 0 ms (100%), Space: 15.2 MB (67.05%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,37 +1,32 @@
 class Solution {
-public:
-    void binarySearch(vector<int>& nums, int target, int left, int right,
-                      int& ans) {
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+    int binarySearch(vector<int>& nums, int target, int low, int high) {
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
             if (nums[mid] == target) {
-                ans = mid;
-                break;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+                return mid;
+            } else if (nums[mid] > target) {
+                high = mid-1;
+            } else
+                low = mid + 1;
         }
+        return -1;
     }
+
+public:
     int search(vector<int>& nums, int target) {
-        int ans = -1, n = nums.size(), pivot = -1;
-        int left = 0;
-        int right = n - 1;
-        while (left < right) {
-            int mid = left + ((right - left) / 2);
-            if ((nums[mid] > nums[right])) {
-                left = mid + 1;
+        int low = 0, high = nums.size() - 1;
+        while (low < high) {
+            int mid = low + ((high - low) >> 1);
+            if (nums[mid] < nums[high]) {
+                high = mid;
             } else {
-                right = mid;
+                low = mid + 1;
             }
         }
-        pivot = left;
-        if (target >= nums[pivot] && target <= nums[n - 1]) {
-            binarySearch(nums, target, pivot, n - 1, ans);
-        } else {
-            binarySearch(nums, target, 0, pivot - 1, ans);
-        }
-        return ans;
+        int pivot = low,res=-1;
+        high = nums.size() - 1;
+        res = binarySearch(nums, target, 0, pivot - 1); // 0..pivot-1
+        res = max(binarySearch(nums, target, pivot, high),res);     // pivot ... high
+        return res;
     }
 };
