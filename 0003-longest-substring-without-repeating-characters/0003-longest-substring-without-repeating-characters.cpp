1ebf2183a19becef0a6f988315aa7feccdadbd2a
Tushar Khatri <132511116+TusharKhatrii@users.noreply.github.com>
2026-03-16 22:48:44 +0500

    Time: 21 ms (20.96%), Space: 14.4 MB (13.22%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,14 +5,13 @@ public:
         int res = 0, n = s.size();
         unordered_set<char> set;
         while (r < n) {
-            if (set.find(s[r]) != set.end()) {
+            while (set.find(s[r]) != set.end()) {
                 set.erase(s[l]);
                 l++;
-            } else {
-                set.insert(s[r]);
-                res = max(res, r - l + 1);
-                r++;
             }
+            set.insert(s[r]);
+            res = max(res, r - l + 1);
+            r++;
         }
         return res;
     }
