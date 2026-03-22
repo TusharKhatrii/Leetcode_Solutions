785f0bc08331c13d4196a1faf70038da45381510
Tushar Khatri <132511116+TusharKhatrii@users.noreply.github.com>
2026-03-22 15:39:24 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,49 +1,36 @@
 class Solution {
-    bool ifZero(unordered_map<char, int>& count1,
-                unordered_map<char, int>& count2) {
-        for (const auto& [key, val] : count1) {
-            auto it = count2.find(key);
-            if (it == count2.end() || it->second < val) {
-                return false;
-            }
-        }
-        return true;
-    }
-
 public:
     string minWindow(string s, string t) {
         if (s.size() < t.size()) {
             return "";
         }
         string res = "";
-        unordered_map<char, int> count1;
-        unordered_map<char, int> count2;
+        vector<int> freq(128,0);
         for (char c : t) {
-            count1[c]++;
+            freq[c]++;
         }
 
-        int windowLength = INT_MAX;
-        int left = 0, right = 0, start, end, old = INT_MAX;
+        int windowLength = t.size(),minWindow=INT_MAX;
+        int left = 0, right = 0,start,ct=0;
         while (right < s.size()) {
-            if (count1.find(s[right]) != count1.end()) {
-                count2[s[right]]++;
-                while (ifZero(count1, count2)) {
-                    old = windowLength;
-                    windowLength = min(windowLength, right - left);
-                    if (count2[s[left]] > 0)
-                        count2[s[left]]--;
-                    left++;
-                    if (old > windowLength) {
-                        start = left - 1;
-                        end = right;
-                    }
-                }
+            if(s[right]>0){
+                ct++;
             }
+            while(ct>=windowLength){
+                start = left;
+                if(s[left]>0){
+                    ct--;
+                }
+                left++;
+            }
+            minWindow = min(minWindow,(right - left));
             right++;
         }
-        while (start <= end && end < s.size()) {
-            res += s[start];
-            start++;
+
+        int i=0;
+        while(i<minWindow){
+            res+=s[start++];
+            i++;
         }
         return res;
     }
