41354ec2e5cd425d2f8c644f07d6cd2db57fac73
Tushar Khatri <132511116+TusharKhatrii@users.noreply.github.com>
2026-03-23 23:20:45 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,13 +1,19 @@
 class Solution {
+    int getMax(vector<int> &freq){
+        int ans = 0;
+        for(int i:freq){
+            ans = max(ans,i);
+        }
+        return ans;
+    }
 public:
     int characterReplacement(string s, int k) {
         vector<int> freq(26, 0);
-        int left = 0, right = 0, maxF = 0, winLen = 0;
+        int left = 0, right = 0, winLen = 0;
         int n = s.size();
         while (right < n) {
             freq[s[right] - 'A']++;
-            maxF = max(maxF, freq[s[right]-'A']);
-            if ((right - left + 1) - maxF > k) {
+            if ((right - left + 1) - getMax(freq) > k) {
                 freq[s[left]-'A']--;
                 left++;
             }
