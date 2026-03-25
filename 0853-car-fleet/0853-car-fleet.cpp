a26d80297cb3a3920719b2d714071b457d13a1d5
Tushar Khatri <132511116+TusharKhatrii@users.noreply.github.com>
2026-03-26 00:00:24 +0500

    Time: 35 ms (86.53%), Space: 105.8 MB (62.5%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        stack<double> s;
+        vector<pair<int, int>> vehicles;
+        int n = position.size();
+        for (int i = 0; i < n; i++) {
+            vehicles.push_back({position[i], speed[i]});
+        }
+        sort(vehicles.begin(), vehicles.end());
+        for (int i = n - 1; i >= 0; i--) {
+            double timeTaken =(double)(target - vehicles[i].first) / vehicles[i].second;
+            if (s.empty()) {
+                s.push(timeTaken);
+            }
+            else if (s.top() < timeTaken) {
+                s.push(timeTaken);
+            }
+        }
+        return s.size();
+    }
+};
