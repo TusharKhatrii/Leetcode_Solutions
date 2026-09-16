169eb2beab569ba1fff6b80fc17b6da035222463
Tushar Khatri <132511116+TusharKhatrii@users.noreply.github.com>
2026-09-16 23:14:12 +0500

    Time: 0 ms (100%), Space: 9.1 MB (55.31%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int maxLen = max(num1.size(), num2.size());
+        string larger = "", smaller = "";
+        if (num1.size() >= num2.size()) {
+            larger = num1;
+            smaller = num2;
+        } else {
+            smaller = num1;
+            larger = num2;
+        }
+        string res(larger.size(), '0');
+
+        int carry = 0, i = larger.size() - 1;
+        for (int j = smaller.size() - 1; j >= 0; j--, i--) {
+            int a = larger[i] - '0';
+            int b = smaller[j] - '0';
+            int sum = a + b + carry;
+            res[i] = (sum % 10) + '0';
+            // cout<<res[i]<<endl;
+            carry = sum / 10;
+        }
+
+        while (i >= 0) {
+            int sum = larger[i] - '0';
+            sum += carry;
+            carry = sum / 10;
+            res[i--] = sum % 10 + '0';
+        }
+        if (carry == 1)
+            res = '1' + res;
+        return res;
+    }
+};
